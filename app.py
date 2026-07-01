3809af7c3a36011b8034a659b9c436040233f357
Darius Kemboi <kemboidarius83@gmail.com>
2026-07-01 13:59:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route("/employees")
-def employees():
-    return render_template("employees.html")
-
-@app.route("/add-employee")
-def add_employee():
-    return render_template("add_employee.html")
+from auth import auth
+app.register_blueprint(auth)
 
 if __name__ == "__main__":
     app.run(debug=True)
